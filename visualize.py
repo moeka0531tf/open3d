46c4916086d6383e644a6ba57c828338dfac7be6
中嶋萌花 <moeka@moeka-4.local>
2020-01-21 22:22:11 +0900

    回転表示機能を追加

--- visualize.py
+++ visualize.py
@@ -2,9 +2,31 @@ import sys
 import open3d as o3d
 import numpy as np
 
-def file_draw(file):
+# 通常の描写
+def normal_visualize(file):
     o3d.visualization.draw_geometries([file], width=640, height=480)
 
+# コールバック関数でオブジェクトを回転させる
+def custom_draw_geometry_with_rotation(file):
+
+     def rotate_view(vis):
+         ctr = vis.get_view_control()
+         ctr.rotate(10.0, 0.0)
+         return False
+
+     o3d.visualization.draw_geometries_with_animation_callback([file], rotate_view, width=640, height=480)
+
+# オブジェクトを任意の方向に回転させて表示
+def rotate_visualize(file, x=0, y=0):
+     vis = o3d.visualization.Visualizer()
+     vis.create_window()
+     vis.add_geometry(file)
+
+     ctr = vis.get_view_control()
+     ctr.rotate(x, y)
+     vis.run()
+     vis.destroy_window()
+     
 if __name__ == "__main__":
 
     file = 'sample.obj'
@@ -15,7 +37,8 @@ if __name__ == "__main__":
     # 頂点数
     vertices = np.asarray(mesh.vertices).shape[0]
 
-    file_draw(mesh)
+    # メッシュファイルを表示
+    normal_visualize(mesh)
 
     # TriangleMesh -> PointCloud　に変換
     # 頂点数で統一が図れる
@@ -26,5 +49,12 @@ if __name__ == "__main__":
 
     # 点の色を変更する(MeshからPCDにすると点の色がついていないため)
     pcd.paint_uniform_color([1, 0, 0])
-    
-    file_draw(pcd)
+
+    # PointCloudを表示
+    normal_visualize(pcd)
+
+    # オブジェクトを回転させて表示
+    custom_draw_geometry_with_rotation(pcd)
+
+    # オブジェクトを任意の角度に回転させて表示
+    rotate_visualize(pcd, 90, 0)
