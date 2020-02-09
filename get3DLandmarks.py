d671a2fc063b2b2ad1d734f31252e765178151b0
中嶋萌花 <moeka@moeka-4.local>
2020-02-09 21:58:23 +0900

    refactor

--- get3DLandmarks.py
+++ get3DLandmarks.py
@@ -10,12 +10,9 @@ import math
 def getRotateImage(objFile, dx, dy = 0, imageName = "faceimage.jpg", width=640, height=480):
     # read file
     mesh = o3d.io.read_triangle_mesh(objFile)
-    # 頂点座標取得
-    # print(np.array(mesh.vertices))
 
-    axis = mesh.get_axis_aligned_bounding_box()
+    # XYZ軸の表示
     coordinate = o3d.geometry.TriangleMesh.create_coordinate_frame(size=200, origin=[0, 0, 0])
-    # print(coordinate)
 
     # 回転行列の計算
     # ラジアンに変換
@@ -33,14 +30,13 @@ def getRotateImage(objFile, dx, dy = 0, imageName = "faceimage.jpg", width=640, 
                    [0, 1, 0],
                    [np.sin(x), 0, np.cos(x)]])
     mesh.rotate(Ry)
-    # 回転後の頂点座標取得
-    # print(np.array(mesh.vertices))
 
     # 回転後表示
     vis = o3d.visualization.Visualizer()
     vis.create_window(width = width, height = height)
     vis.add_geometry(mesh)
-    vis.add_geometry(coordinate)
+    # 軸を表示したい場合は下記コメントアウトを外す
+    # vis.add_geometry(coordinate)
     vis.run()
 
     # 画像保存
@@ -51,10 +47,10 @@ def getRotateImage(objFile, dx, dy = 0, imageName = "faceimage.jpg", width=640, 
     pinhole = ctr.convert_to_pinhole_camera_parameters()
     # カメラ外部パラメータ
     extrinsic = pinhole.extrinsic
-    # print(extrinsic)
+
+    # カメラ内部パラメータ取得
     pi = pinhole.intrinsic
     im = pi.intrinsic_matrix
-    # print(im)
 
     vis.destroy_window()
 
@@ -110,8 +106,8 @@ def getObjectPoints(dx, dy, cameraPoints, extrinsic, points):
     x = math.radians(dx)
     y = math.radians(dy)
 
+    # 焦点距離
     focal_length = 415.69219382
-    finalPoints = []
 
     # x軸回転
     Rx = np.array([[1, 0, 0],
@@ -124,6 +120,8 @@ def getObjectPoints(dx, dy, cameraPoints, extrinsic, points):
                    [np.sin(x), 0, np.cos(x)]])
 
     # 最終的な特徴点
+    finalPoints = []
+
     for i in range(0, len(cameraPoints)):
         array = np.array(points[i])
         array = np.insert(array, 3, 1)
@@ -140,21 +138,18 @@ def getObjectPoints(dx, dy, cameraPoints, extrinsic, points):
         point = np.dot(np.linalg.inv(Ry), objectPoint)
         points.append(point.T.reshape(3,).tolist())
 
-    # 描画用のポイントを整備する
+    # 移動後のポイントを描画する
     finalArray = np.array(finalPoints)
-
-    print(finalArray)
     pcd = o3d.geometry.PointCloud()
-
     pcd.points = o3d.utility.Vector3dVector(finalArray)
-    o3d.visualization.draw_geometries([pcd])
+    o3d.visualization.draw_geometries([pcd], width=640, height=480)
     return finalArray
 
 
 if __name__ == "__main__":
 
     file = 'file'
-    imageName = 'faceImage-15degree.jpg'
+    imageName = 'faceImage.jpg'
 
     # 画像サイズ
     width = 640
